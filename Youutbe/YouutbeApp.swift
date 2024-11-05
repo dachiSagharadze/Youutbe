38740984cf60ef791acc07432c8af27f603730ed
dachiSagharadze <dachi.sagaradze@gmail.com>
2024-11-05 16:16:44 +0400

    Initial Commit

---
+++ Youutbe/YouutbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouutbeApp.swift
+//  Youutbe
+//
+//  Created by Dachi Sagharadze on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct YouutbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouutbeTests/YouutbeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouutbeTests.swift
+//  YouutbeTests
+//
+//  Created by Dachi Sagharadze on 05.11.24.
+//
+
+import Testing
+@testable import Youutbe
+
+struct YouutbeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
